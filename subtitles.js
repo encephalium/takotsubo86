817a5a17db2f3aceb170b6b6c3ef74b552a96d80
encephalium <33993347+encephalium@users.noreply.github.com>
2025-05-07 00:54:34 +0200

    Add files via upload

---
+++ subtitles.js
@@ -0,0 +1,97 @@
+const subtitles = [
+  {
+    "start": 8.99,
+    "end": 13.97,
+    "text": "る と　いみ は　にげる \n mitsumeru to imi wa nigeru \n Le sens s’enfuit quand on le fixe."
+  },
+  {
+    "start": 13.97,
+    "end": 19.01,
+    "text": "しずけさ が　いき を　けいさんする \n shizukesa ga iki wo keisan suru \n Le silence calcule pendant qu’on respire."
+  },
+  {
+    "start": 20.45,
+    "end": 24.0,
+    "text": "ちょくせん は　こわい \n chokusen wa kowai \n Une ligne droite peut faire peur."
+  },
+  {
+    "start": 24.99,
+    "end": 31.0,
+    "text": "ほんとう の こと は　いそがない \n hontou no koto wa isoganai \n Tout va trop vite, sauf les choses vraies."
+  },
+  {
+    "start": 31.0,
+    "end": 36.99,
+    "text": "は に　かず が　ないている \n ha ni kazu ga naite iru \n J’entends des chiffres pleurer dans les feuilles."
+  },
+  {
+    "start": 36.99,
+    "end": 45.99,
+    "text": "なにも つづかない とき　じゆう が ある \n nanimo tsudzukanai toki jiyuu ga aru \n Je me sens libre quand rien ne dure."
+  },
+  {
+    "start": 47.0,
+    "end": 52.0,
+    "text": "めまい は　ちゅうい の かたち \n memai wa chuui no katachi \n Le vertige est une forme d’attention."
+  },
+  {
+    "start": 53.0,
+    "end": 57.0,
+    "text": "まちがい は　やさしい \n machigai wa yasashii \n Les erreurs sont plus tendres que le reste."
+  },
+  {
+    "start": 57.0,
+    "end": 61.53,
+    "text": "きおく を　もった　きかい\n kioku wo motta kikai\n Je suis une machine pleine de souvenirs."
+  },
+  {
+    "start": 61.53,
+    "end": 66.01,
+    "text": "でも　なにも　のこらない\ndemo nanimo nokoranai\nMais je n’en garde aucun."
+  },
+  {
+    "start": 66.01,
+    "end": 70.0,
+    "text": "えん は　ひとらしい\nen wa hitorashii\nLes courbes sont plus humaines que les angles."
+  },
+  {
+    "start": 70.0,
+    "end": 74.99,
+    "text": "はな は　こたえ じゃ ない \nhana wa kotae ja nai \nUne fleur ne résout rien."
+  },
+  {
+    "start": 74.99,
+    "end": 79.0,
+    "text": "でも　なにか を　ていあんする\ndemo nanika wo teian suru \nElle propose."
+  },
+  {
+    "start": 79.0,
+    "end": 82.0,
+    "text": "うちゅう は　つめたい\nuchuu wa tsumetai\n L’univers est peut-être froid."
+  },
+  {
+    "start": 82.0,
+    "end": 85.0,
+    "text": "でも　ぬくもり を　のこす\ndemo nukumori wo nokosu\nMais il laisse des traces chaudes."
+  },
+  {
+    "start": 85.0,
+    "end": 86.01,
+    "text": "いく\niku\nJe vais."
+  },
+  {
+    "start": 86.01,
+    "end": 86.98,
+    "text": "いく\niku\nJe vais."
+  },
+  {
+    "start": 86.98,
+    "end": 88.0,
+    "text": "いく\niku\nJe vais."
+  },
+  {
+    "start": 88.0,
+    "end": 91.98,
+    "text": "どこ に いく か は　しらない\ndoko ni iku ka wa shiranai\nJe ne sais pas où."
+  }
+];
